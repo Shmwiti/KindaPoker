adb3dd230cddf7f75888d0982d32b3df98857243
Shmwiti <87014612+Shmwiti@users.noreply.github.com>
2024-03-05 12:24:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let gameHasEnded = false;
+const STARTING_POKER_CHIPS = 100; // points
+const PLAYERS = 3; 
+const NO_OF_STARTING_CARDS = 2;
+
+
+let playerOnePoints = STARTING_POKER_CHIPSs ;
+let playerTwoPoints = STARTING_POKER_CHIPSs ;
+let playerThreePoints = STARTING_POKER_CHIPSs ;
+
+console.log(`Welcome to Texas Hold'em. The championship title will be awarded to one of these three players: ${playerOneName}, ${playerTwoName} and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS} in their pot. We have an exciting game ahead of us. May the best player win!`)
+
+playerOnePoints -=50;
+playerTwoPoints -=25;
+playerThreePoints +=75;
+
+let playerOneName = "Chloe";
+let playerTwoName = "Jasmine";
+let playerThreeName = "Ben";
+
+
+gameHasEnded = ((playerOnePoints + playerTwoPoints) == 0) || // three has won
+               ((playerTwoPoints + playerThreePoints) == 0) ||  // one has won
+               ((playerOnePoints + playerThreePoints) == 0);  // two has won 
+console.log("Game has ended: ", gameHasEnded);
+
+
